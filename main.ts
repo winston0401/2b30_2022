3b417d359e2480c9523f343524b3a3dd0fbbe22b
winston0401 <115680357+winston0401@users.noreply.github.com>
2022-10-16 00:20:22 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,48 @@
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 2470, 255, 0, 2420, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+basic.forever(function () {
+	
+})
