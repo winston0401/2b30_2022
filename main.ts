4644a83cbc2689a0a918106ea769d8d36c69dca3
winston0401 <115680357+winston0401@users.noreply.github.com>
2022-10-20 10:38:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+let button = 0
 input.onButtonPressed(Button.A, function () {
+    button = 0
     music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 2470, 255, 0, 2420, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     basic.showLeds(`
         . . . . .
